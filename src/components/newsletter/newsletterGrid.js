20152dcb81e44d18fffdf450623ee8f96f342901
Matthew James <jamesmmatt@gmail.com>
2019-01-04 20:49:05 -0600

    mapped over a newsletter JSON item to the latest newsletter component

--- src/components/newsletter/newsletterGrid.js
+++ src/components/newsletter/newsletterGrid.js
@@ -6,11 +6,20 @@ import NewsletterLatest from './newsletterLatest';
 
 class NewsletterGrid extends Component {
     render() {
+
+        const latest = {
+            _id: '115',
+            title: 'Happy Holidays',
+            body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sed quam nibh. Etiam ac tellus imperdiet, vestibulum nibh vitae, lobortis tellus. Nunc gravida efficitur nulla id ullamcorper. Vivamus at leo interdum, rhoncus nisi at, malesuada lacus. Quisque semper non augue a iaculis. Quisque a nisl velit. Nulla magna urna, laoreet sit amet lorem non, porttitor congue justo. Phasellus fermentum vulputate sem, pharetra tristique neque auctor at. Aliquam eleifend nibh ut mattis dignissim. Sed tempus nec tellus quis porta. Mauris id egestas massa. Etiam fringilla, est egestas ultrices vestibulum, ligula metus cursus massa, vitae imperdiet purus nunc id urna. Duis ipsum odio, iaculis eu viverra a, rhoncus posuere dui. Nulla quam nisl, pulvinar malesuada finibus et, rutrum in diam. Sed viverra eros quis hendrerit convallis.',
+            date: new Date(),
+            imageUrl: 'http://via.placeholder.com/960x258'
+        }
+
         return (
             <div className='newsletter-grid'>
                 <NewsletterBox date={new Date()}/>     
                 <NewsletterArchive/>  
-                <NewsletterLatest/>
+                <NewsletterLatest { ...latest }/>
             </div>
         )
     }
--- src/components/newsletter/newsletterLatest.js
+++ src/components/newsletter/newsletterLatest.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 
 class NewsletterLatest extends Component {
     render() {
+        const { title, imageUrl, body } = this.props
         return (
             <div className='newsletter-latest'>
-                <h1 className='newsletter-latest__title'>Title goes here</h1>
-                <img className='newsletter-latest__image' src='http://via.placeholder.com/960x258'/>
+                <h1 className='newsletter-latest__title'>{title}</h1>
+                <img className='newsletter-latest__image' src={imageUrl}/>
                 <div className='newsletter-latest__body'>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sed quam nibh. Etiam ac tellus imperdiet, vestibulum nibh vitae, lobortis tellus. Nunc gravida efficitur nulla id ullamcorper. Vivamus at leo interdum, rhoncus nisi at, malesuada lacus. Quisque semper non augue a iaculis. Quisque a nisl velit. Nulla magna urna, laoreet sit amet lorem non, porttitor congue justo. Phasellus fermentum vulputate sem, pharetra tristique neque auctor at. Aliquam eleifend nibh ut mattis dignissim. Sed tempus nec tellus quis porta. Mauris id egestas massa. Etiam fringilla, est egestas ultrices vestibulum, ligula metus cursus massa, vitae imperdiet purus nunc id urna. Duis ipsum odio, iaculis eu viverra a, rhoncus posuere dui. Nulla quam nisl, pulvinar malesuada finibus et, rutrum in diam. Sed viverra eros quis hendrerit convallis.</p>
+                    <p>{body}</p>
                 </div>
             </div>
         )
