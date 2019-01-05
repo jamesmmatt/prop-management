cee46eb1c47d3aa7d78b0c940fd512db5862292a
Matthew James <jamesmmatt@gmail.com>
2019-01-04 20:39:21 -0600

    styled newsletter latest and applied row gaps to the newsletter grid and added a functino to make single digit dates lead with a zero

--- src/components/newsletter/newsletterBox.js
+++ src/components/newsletter/newsletterBox.js
@@ -4,11 +4,18 @@ const months = [
     'Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'
 ]
 
+let isDateBelowZero = (dateNum) => {
+    if(dateNum < 10) {
+        let modDateNum = String('0' + dateNum);
+        return modDateNum;
+    }    
+}
+
 
 export default function NewsletterBox({date}) {
     return (
         <div className='newsletter-box'>
-            <div className='newsletter-box__day'>{date.getDate()}</div>
+            <div className='newsletter-box__day'>{isDateBelowZero(date.getDate())}</div>
             <div className='newsletter-box__month-year'>{months[date.getMonth()]} {date.getFullYear()}</div>
             <div className='newsletter-box__point'></div>
         </div>
--- src/components/newsletter/newsletterLatest.js
+++ src/components/newsletter/newsletterLatest.js
@@ -7,7 +7,7 @@ class NewsletterLatest extends Component {
                 <h1 className='newsletter-latest__title'>Title goes here</h1>
                 <img className='newsletter-latest__image' src='http://via.placeholder.com/960x258'/>
                 <div className='newsletter-latest__body'>
-                    <p>Lorem Ispum</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sed quam nibh. Etiam ac tellus imperdiet, vestibulum nibh vitae, lobortis tellus. Nunc gravida efficitur nulla id ullamcorper. Vivamus at leo interdum, rhoncus nisi at, malesuada lacus. Quisque semper non augue a iaculis. Quisque a nisl velit. Nulla magna urna, laoreet sit amet lorem non, porttitor congue justo. Phasellus fermentum vulputate sem, pharetra tristique neque auctor at. Aliquam eleifend nibh ut mattis dignissim. Sed tempus nec tellus quis porta. Mauris id egestas massa. Etiam fringilla, est egestas ultrices vestibulum, ligula metus cursus massa, vitae imperdiet purus nunc id urna. Duis ipsum odio, iaculis eu viverra a, rhoncus posuere dui. Nulla quam nisl, pulvinar malesuada finibus et, rutrum in diam. Sed viverra eros quis hendrerit convallis.</p>
                 </div>
             </div>
         )
