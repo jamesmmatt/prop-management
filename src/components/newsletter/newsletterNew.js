ae258d09709e7e99d39982ce9618d874e008eb5b
Matthew James <jamesmmatt@gmail.com>
2019-01-10 13:35:48 -0600

    setup createnewsletter action and connected the newsletternew component to redux and our actions

--- src/components/newsletter/newsletterNew.js
+++ src/components/newsletter/newsletterNew.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
+
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
 import NewNewsletterForm from './newsletterNewForm';
+
+
 class NewNewsletter extends Component {
 
     onSubmit = (fields) => {
+            const { title, body, image  } = fields;
 
-        // if(button == 'submit') {
-        //   // save new newsletter on the backend. perform a post request here.
-        //   console.log('trying to submit to backend.');
-        // } 
-        this.props.history.push('/dashboard')
+            var formData = new FormData();
+            formData.append('title', title);
+            formData.append('body', body);
+            formData.append('image', image);
+
+            this.props.createNewNewsletter(this.props._id, formData, () => {
+                this.props.history.push('/dashboard');
+        })
     }
 
     onCancel = () => {
@@ -31,4 +41,7 @@ class NewNewsletter extends Component {
         )
     }
 }
+
+NewNewsletter = connect(null, actions)(NewNewsletter);
+
 export default NewNewsletter;
